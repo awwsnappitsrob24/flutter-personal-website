8f50cd35f0ea0d1dd5aac88b7202e4107fe2ac8c
Robien <robienallen@yahoo.com>
2022-02-12 02:41:32 -0700

    set up utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String myName = "Robien Marquez";
+  static const String myTitle = "Software Developer";
+  static const String myPhoneNumber = "(310)-755-4434";
+  static const String myEmail = "robienallen@yahoo.com";
+  static const String facebookProfileUrl =
+      "https://www.facebook.com/rob21swishh/";
+}
